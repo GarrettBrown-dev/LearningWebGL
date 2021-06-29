3ac854ecdaa40117a06ac06beb5a8378278807cf
Garrett <gman9mm@live.com>
2021-06-29 14:43:23 -0700

    Change shape to torus.

--- sketch.js
+++ sketch.js
@@ -10,12 +10,13 @@ function draw() {
   rectMode(CENTER);
   noStroke();
   fill(0, 0, 255);
+  //translate(0, 0, mouseX); //zooms with mouse.
+  //translate(mouseX - width / 2, mouseY - height / 2); moves with mouse.
   rotateX(angle);
   rotateY(angle * 0.3);
   rotateZ(angle * 1.2);
   //rect(0, 0, 150, 150);
-  translate(mouseX, mouseY);
-  box(10, 100, 50);
+  torus(100, 10);
 
   angle += 0.05;
 }

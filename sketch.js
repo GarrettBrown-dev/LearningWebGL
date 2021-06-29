b84cfcdcb32888d6fb379640933851020d7a4ce8
Garrett <gman9mm@live.com>
2021-06-29 14:38:43 -0700

    Plate now zooms in relation to mouse position.

--- sketch.js
+++ sketch.js
@@ -11,7 +11,11 @@ function draw() {
   noStroke();
   fill(0, 0, 255);
   rotateX(angle);
-  rect(0, 0, 150, 150);
+  rotateY(angle * 0.3);
+  rotateZ(angle * 1.2);
+  //rect(0, 0, 150, 150);
+  translate(mouseX, mouseY);
+  box(10, 100, 50);
 
-  angle += 0.07;
+  angle += 0.05;
 }

c7208e3c7fa3da8e638a2ecb41ba140606fd5079
Garrett <gman9mm@live.com>
2021-07-24 14:37:31 -0700

    Add normal metrial.

--- sketch.js
+++ sketch.js
@@ -8,13 +8,14 @@ function draw() {
   background(175);
 
   rectMode(CENTER);
-  noStroke();
-  fill(0, 0, 255);
+  stroke(0);
+  //fill(200, 0, 255);
+  normalMaterial(); //Used predominantly for debugging.
   //translate(0, 0, mouseX); //zooms with mouse.
   //translate(mouseX - width / 2, mouseY - height / 2); moves with mouse.
   rotateX(angle);
   rotateY(angle * 0.3);
-  rotateZ(angle * 1.2);
+  rotateZ(angle * 1.1);
   //rect(0, 0, 150, 150);
   torus(100, 10);
 

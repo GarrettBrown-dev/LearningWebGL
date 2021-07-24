92ae8a578bcf5790606b74e04943c889efa89a1a
Garrett <gman9mm@live.com>
2021-07-24 15:26:47 -0700

    Mess with point lights.

--- sketch.js
+++ sketch.js
@@ -5,19 +5,23 @@ function setup() {
 }
 
 function draw() {
+  pointLight(0, 0, 255, -200, 0, 250);
+  pointLight(255, 0, 0, 200, 0, 250);
   background(175);
 
-  rectMode(CENTER);
-  stroke(0);
-  //fill(200, 0, 255);
-  normalMaterial(); //Used predominantly for debugging.
-  //translate(0, 0, mouseX); //zooms with mouse.
-  //translate(mouseX - width / 2, mouseY - height / 2); moves with mouse.
   rotateX(angle);
   rotateY(angle * 0.3);
-  rotateZ(angle * 1.1);
-  //rect(0, 0, 150, 150);
-  torus(100, 10);
+  rotateZ(angle * 1.2);
 
-  angle += 0.05;
+  noStroke();
+  ambientMaterial(255);
+  sphere(100);
+
+  angle += 0.03;
 }
+
+//rect(0, 0, 150, 150);
+//fill(200, 0, 255);
+//normalMaterial(); //Used predominantly for debugging.
+//translate(0, 0, mouseX); //zooms with mouse.
+//translate(mouseX - width / 2, mouseY - height / 2); moves with mouse.
